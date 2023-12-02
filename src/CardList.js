70d99e2da09d885a5a76065426a0c049c06f1ad0
Siddhesh <gaonkarsiddhesh0907@gmail.com>
2023-12-02 20:36:49 +0530

    pre next navigation

--- src/CardList.js
+++ src/CardList.js
@@ -1,27 +1,48 @@
 import React from "react";
 import Card from "./Card";
 import "./CardList.css";
-import employees from "./emp-data.json";
+import employee from "./emp-data.json";
 
 function CardList() {
+  let page_no = 0;
+  let per_page = 3;
+  let start_index = 0;
+  let end_index = 3;
+  let employees = employee.slice(start_index, end_index);
+
+  let pre_page = () => {
+    page_no -= 1;
+    if (page_no >= 0) {
+      start_index = page_no * per_page;
+      end_index = start_index + per_page;
+      console.log(start_index, end_index);
+      employee = employee.slice(start_index, end_index);
+    }
+  };
+
+  let next_page = () => {
+    page_no += 1;
+    if (page_no >= 0) {
+      start_index = page_no * per_page;
+      end_index = start_index + per_page;
+      console.log(start_index, end_index);
+      employee = employee.slice(start_index, end_index);
+    }
+  };
+
   return (
     <>
-      <div className="parent">
-        {/* <Card {...employee[0]} /> */}
-
-        {/* <Card employee_data={employee[0]} />
-      <Card employee_data={employee[1]} />
-      <Card employee_data={employee[2]} />
-      <Card employee_data={employee[3]} /> */}
-
-        {employees.map((employee) => {
-          return <Card {...employee} />;
-        })}
+      <div className="card-list">
+        <div className="parent">
+          {employees.map((employee) => {
+            return <Card {...employee} key={employee.id} />;
+          })}
+        </div>
       </div>
 
       <div className="nav">
-        <button className="button">Pre</button>
-        <button className="button">Next</button>
+        <button className="button" onClick={pre_page}>Pre</button>
+        <button className="button" onClick={next_page}>Next</button>
       </div>
     </>
   );
